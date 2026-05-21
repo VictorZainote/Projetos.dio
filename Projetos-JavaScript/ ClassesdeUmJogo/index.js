1a16c1069ed530c014bee0fa93c213addbc0d507
Victor Hugo Azevedo Alves Zainote <132853729+VictorZainote@users.noreply.github.com>
2026-05-21 17:18:02 -0300

    feat: criação de classes de heróis e interação com a função de ataque

---
+++ Projetos-JavaScript/ ClassesdeUmJogo/index.js
@@ -0,0 +1,39 @@
+class Heroi {
+    constructor(nome, idade, tipo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo.toLowerCase(); 
+    }
+
+    atacar() {
+        let ataque = "";
+
+        if (this.tipo === "mago") {
+            ataque = "magia";
+        } else if (this.tipo === "guerreiro") {
+            ataque = "espada";
+        } else if (this.tipo === "monge") {
+            ataque = "artes marciais";
+        } else if (this.tipo === "ninja") {
+            ataque = "shuriken";
+        } else {
+            ataque = "um ataque básico"; 
+        }
+
+        console.log(`o ${this.tipo} atacou usando ${ataque}`);
+    }
+}
+
+const heroi1 = new Heroi("Gandalf", 150, "mago");
+const heroi2 = new Heroi("Hercules", 35, "guerreiro");
+
+
+heroi1.atacar(); 
+heroi2.atacar();
+
+const nomeHeroi = gets();
+const idadeHeroi = parseInt(gets());
+const tipoHeroi = gets();
+
+const heroiPersonalizado = new Heroi(nomeHeroi, idadeHeroi, tipoHeroi);
+heroiPersonalizado.atacar(); 
